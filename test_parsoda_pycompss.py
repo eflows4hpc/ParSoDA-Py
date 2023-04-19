564b3690bcaf7c447383a5a1ed19074bdc35a8ea
sv.giampa <sv.giampa@gmail.com>
2023-04-19 14:11:17 +0000

    Code configured for tests on cluster

--- test_parsoda_pycompss.py
+++ test_parsoda_pycompss.py
@@ -1,14 +1,16 @@
 import argparse
 import os
+import math
 from datetime import datetime
 
 apps_list = [
     "trajectory_mining_pycompss", 
     #"emoji_polarization_pycompss",
 ]
-cores_list = [128]
-chunk_sizes = [128]
-test_num = 1
+cores_list = [256,128,64,32,16,8]
+#dataset_size = 38818;
+chunk_sizes = [170]
+test_num = 3
 
 def parse_commandline():
     parser = argparse.ArgumentParser("ParSoDA-PyCOMPSs test")
@@ -60,14 +62,16 @@ if __name__ == '__main__':
                     
                     jhome = os.environ['JAVA_HOME']
                     print(f"JAVA_HOME={jhome}")
+                   # current_chunk_size = math.ceil(dataset_size/cores);
                     cmd = (
                         f"runcompss --python_interpreter=python3 "
-                        f"--resources=./config/resources.xml "
+                        f"--resources=./config/resources-{cores}.xml "
                         f"--project=./config/project-{cores}cores.xml "
                         f"--library_path=\"{jhome}\" "
-                        f"--jvm_workers_opts=\"-Xmx25g\" "
+                        f"--jvm_workers_opts=\"-Xmx3900M\" "
                         #f"--base_log_dir=\"{app_logs_dir}\" "
-                        f"./src/{app}.py --chunk-size {chunk_size} "
+                        #f"./src/{app}.py --chunk-size {current_chunk_size} "
+                        f"./src/{app}.py --partitions {cores} "
                         f" > {app_logs_dir}/{app}.{cores}cores.chunk{chunk_size}.test{test_index}.log"
                     )
                     print(f"command: {cmd}")
@@ -80,34 +84,51 @@ if __name__ == '__main__':
                                 fields = report_line.split(";")
                                 
                                 #app_name = fields[0]
-                                num_partitions_given += int(fields[1])
-                                chunk_size_given += int(fields[2])
-                                crawling_time += int(fields[3])
-                                filter_time += int(fields[4])
-                                map_time += int(fields[5])
-                                split_time += int(fields[6])
-                                reduce_time += int(fields[7])
-                                analysis_time += int(fields[8])
-                                visualization_time += int(fields[9])
-                                filter_to_reduce_time += int(fields[10])
-                                total_execution_time += int(fields[11])
-                                total_time += int(fields[12])
+                                # num_partitions_given += int(fields[1])
+                                # chunk_size_given += int(fields[2])
+                                # crawling_time += int(fields[3])
+                                # filter_time += int(fields[4])
+                                # map_time += int(fields[5])
+                                # split_time += int(fields[6])
+                                # reduce_time += int(fields[7])
+                                # analysis_time += int(fields[8])
+                                # visualization_time += int(fields[9])
+                                # filter_to_reduce_time += int(fields[10])
+                                # total_execution_time += int(fields[11])
+                                # total_time += int(fields[12])
+
+                                num_partitions_given = int(fields[1])
+                                chunk_size_given = int(fields[2])
+                                crawling_time = int(fields[3])
+                                filter_time = int(fields[4])
+                                map_time = int(fields[5])
+                                split_time = int(fields[6])
+                                reduce_time = int(fields[7])
+                                analysis_time = int(fields[8])
+                                visualization_time = int(fields[9])
+                                filter_to_reduce_time = int(fields[10])
+                                total_execution_time = int(fields[11])
+                                total_time = int(fields[12])
+
+                                with open(test_results_file, "a") as f:
+                                    f.write(f"{app};{cores};{num_partitions_given};{chunk_size_given};{crawling_time};{filter_time};{map_time};{split_time};{reduce_time};{analysis_time};{visualization_time};{filter_to_reduce_time};{total_execution_time};{total_time}\n".replace(".", ","))
+
                         except: 
                             pass
                     
 
-                num_partitions_given /= test_num
-                chunk_size_given /= test_num
-                crawling_time /= test_num
-                filter_time /= test_num
-                map_time /= test_num
-                split_time /= test_num
-                reduce_time /= test_num
-                analysis_time /= test_num
-                visualization_time /= test_num
-                filter_to_reduce_time /= test_num
-                total_execution_time /= test_num
-                total_time /= test_num
+                # num_partitions_given /= test_num
+                # chunk_size_given /= test_num
+                # crawling_time /= test_num
+                # filter_time /= test_num
+                # map_time /= test_num
+                # split_time /= test_num
+                # reduce_time /= test_num
+                # analysis_time /= test_num
+                # visualization_time /= test_num
+                # filter_to_reduce_time /= test_num
+                # total_execution_time /= test_num
+                # total_time /= test_num
 
-                with open(test_results_file, "a") as f:
-                    f.write(f"{app};{cores};{num_partitions_given};{chunk_size_given};{crawling_time};{filter_time};{map_time};{split_time};{reduce_time};{analysis_time};{visualization_time};{filter_to_reduce_time};{total_execution_time};{total_time}\n".replace(".", ","))
+                #with open(test_results_file, "a") as f:
+                #    f.write(f"{app};{cores};{num_partitions_given};{chunk_size_given};{crawling_time};{filter_time};{map_time};{split_time};{reduce_time};{analysis_time};{visualization_time};{filter_to_reduce_time};{total_execution_time};{total_time}\n".replace(".", ","))
