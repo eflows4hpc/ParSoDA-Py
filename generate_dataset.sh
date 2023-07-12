750ca3b38f9a7e9e691f5ef6040fac98b9d06194
sv.giampa <sv.giampa@gmail.com>
2023-07-12 14:58:16 +0000

    Update to generate_dataset.sh

--- generate_dataset.sh
+++ generate_dataset.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=src python3 src/parsoda/utils/dataset_generator.py 10m resources/input/RomeRoIs.kml resources/input/synthetic_40m.json
+PYTHONPATH=src python3 parsoda/utils/dataset_generator.py 10m resources/input/RomeRoIs.kml resources/input/synthetic_40m.json
