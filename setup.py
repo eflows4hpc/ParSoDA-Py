931ba81226e1da02034fb6fc3c18a393c6fe9021
sv.giampa <sv.giampa@gmail.com>
2023-07-13 13:39:29 +0000

    Added license and more precise metadata to the setup.py

--- setup.py
+++ setup.py
@@ -10,20 +10,25 @@ with open("requirements.txt", "r") as f:
 setup(
     name = "ParSoDA",
     version = "1.1.0",
-    author = "", #TODOS
-    author_email = "", #TODO
-    description = ("ParSoDA: Parallel Social Data Analytics library, running on multiple environments"),
-    license = "", #TODO
-    keywords =  "social data analysis scalability parallel big-data social-media social-networks distributed-computing"
-                "media networks distributed computing trajectory mining pattern roi region of interest point of interest",
+    author = "Loris Belcastro, Salvatore Giampà, Fabrizio Marozzo, Domenico Talia, Paolo Trunfio",
+    author_email = "belcastro@dtoklab.com, giampa@dtoklab.com, marozzo@dtoklab.com, talia@dtoklab.com, trunfio@dtoklab.com",
+    description = ("ParSoDA: a Parallel Social Data Analytics library"),
+    keywords =  "social,data,analysis,scalability,parallel,big data,social media,social networks,distributed computing"
+                "media,networks,distributed,computing,trajectory,mining,pattern,roi,region of interest,point of interest",
     url = "https://github.com/eflows4hpc/parsoda",
     packages=find_packages(where='.'),
-    long_description=readme,
     python_requires=">=3.8.0",
     install_requires=requirements,
-    classifiers=[ #TODO
-        #"Development Status :: 3 - Alpha",
-        "Topic :: Distributed Computing", 
-        #"License :: OSI Approved :: BSD License",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license_files = ("LICENSE"),
+    license = "GNU General Public License v3 (GPLv3)",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Distributed Computing",
     ],
 )
